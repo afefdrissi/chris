2f5ad697f99fd3b40424bd6708a5a85e1d2969f9
Afef <drissiafef1@gmail.com>
2019-04-08 10:51:13 +0100

    js and improved seo

--- info.js
+++ info.js
@@ -1,4 +1,11 @@
 var recsInfo=[
+       
+   {
+    pic:"woody.jpg",
+    name:"Woody Woodward",
+    pos:"CEO, M.O.N.E.Y. Matrix",
+    quote:"When you are looking for a creative genius look no further than Chris. I have worked with him off and on for over 15 yrs now. He has helped me land some of my biggest clients. If you ever get the chance to work with him you will not be disappointed."
+},
     {pic:"0.jpeg",
     name:"Thomas McFarland Jr.",
     pos:"Research and Development Manager",
@@ -11,6 +18,7 @@ var recsInfo=[
     quote:"Chris Dortch is a man of integrity, vision, and leadership with incredible amounts of business knowledge and experience, which ranges from raising venture capital to selling company shares. While Chris regularly leads organized board meetings and prepares various company reports, he is a rainmaker and one that generates opportunity wherever he goes. Chris has orchestrated multiple very successful win-win deals involving both domestic and international negotiations. Chris has many business contacts and friends, of which I am both."
 }
    ,
+
    {
        pic:"",
        name:"",
