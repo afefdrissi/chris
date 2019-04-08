d21dc40c78962455ac27d0906cd8a5e10707e20f
Afef <drissiafef1@gmail.com>
2019-04-08 10:26:49 +0100

    js

--- index.js
+++ index.js
@@ -8,3 +8,21 @@ function currentLink(){
         this.classList.add("activeLink");
 
 }
+function recs(){
+    recsInfo.forEach(x=>{
+        var rec="<div class='testimonial'><div class='testi'>";
+        if (x.pic!=""){
+            rec+="<img src='Links/"+x.pic+"'>";
+        }
+        ////
+        rec+="<div>"+"<div class='name'>"+x.name+"</div>"+
+        "<div class='pos'>"+x.pos+"</div>"+
+        
+        "</div></div>";
+        //quote
+        rec+="<q>"+x.quote+"</q></div>";
+        document.querySelector("#rec .content").innerHTML+=rec;
+    })
+    
+}
+recs();
---
+++ info.js
@@ -0,0 +1,20 @@
+var recsInfo=[
+    {pic:"0.jpeg",
+    name:"Thomas McFarland Jr.",
+    pos:"Research and Development Manager",
+    quote:"Chris is an outstanding media professional and manager....I have worked closely with him on some very tough projects....crazy schedules, moving design requirements, picky customers--And he always nails it. One of the best I have ever seen!"
+   },
+   {
+    pic:"0-2.jpeg",
+    name:"Ron White",
+    pos:"CTO Global Archive, Inc.",
+    quote:"Chris Dortch is a man of integrity, vision, and leadership with incredible amounts of business knowledge and experience, which ranges from raising venture capital to selling company shares. While Chris regularly leads organized board meetings and prepares various company reports, he is a rainmaker and one that generates opportunity wherever he goes. Chris has orchestrated multiple very successful win-win deals involving both domestic and international negotiations. Chris has many business contacts and friends, of which I am both."
+}
+   ,
+   {
+       pic:"",
+       name:"",
+       pos:"",
+       quote:""
+   }
+]
